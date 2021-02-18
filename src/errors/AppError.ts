36f3f8fc631af516249c1514ed5dd8f3ae952deb
Bruno Farias <begfarias@gmail.com>
2021-02-18 15:37:17 -0300

    Add files

---
+++ src/errors/AppError.ts
@@ -0,0 +1,12 @@
+class AppError {
+  public readonly message: string;
+
+  public readonly satusCode: number;
+
+  constructor(message: string, statusCode = 400) {
+    this.message = message;
+    this.satusCode = statusCode;
+  }
+}
+
+export default AppError;
